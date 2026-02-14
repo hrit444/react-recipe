89291c6eadb7a285277f237a496303b5d33c6aa8
hritam04.samanta@gmail.com <hritam04.samanta@gmail.com>
2026-02-14 13:27:49 +0530

    hero page's image changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import "./Hero.css";
 
 const Hero = () => {
   return (
-    <div className="hero h-full overflow-hidden">
+    <div className="hero bg-[#FBF6EC] h-screen w-screen overflow-hidden">
       <div className="text">
         <h1>
           Healthy <span>Eating is </span> <br className="inline lg:hidden" />{" "}
@@ -11,7 +11,7 @@ const Hero = () => {
           <span>an</span> <span className="oText">Important</span> part <br />{" "}
           of Lifestyle
         </h1>
-        <img className="mImg" src="/Images/herofood.png" alt="" />
+        <img className="mImg" src="https://images.pexels.com/photos/28371798/pexels-photo-28371798.jpeg" alt="" />
         <p>We prepare delicious food recipes for you. <br className="hidden lg:inline"/> We are always here!</p>
         <div className="btn flex justify-center align-center lg:justify-start gap-[4vw] lg:gap-[3vw] ">
           <NavLink
@@ -28,7 +28,7 @@ const Hero = () => {
           </NavLink>
         </div>
       </div>
-      <img className="dImg" src="/Images/herofood.png" alt="" />
+      <img className=".dImg hidden xl:block h-[40vw] w-[30vw] bg-center object-cover rounded-[2vw]" src="https://images.pexels.com/photos/28371797/pexels-photo-28371797.jpeg" alt="" />
     </div>
   );
 };
